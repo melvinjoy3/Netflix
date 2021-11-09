1da288889bdd7e6a7d4277c63d7d02339d71f241
melbin <melvinjoy8@gmail.com>
2021-11-09 13:29:41 +0530

    comment changed

--- src/Compoents/RowPost/RowPost.js
+++ src/Compoents/RowPost/RowPost.js
@@ -26,7 +26,7 @@ const opts = {
     height: '390',
     width: '100%',
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
+         
       autoplay: 1,
     },
 };
